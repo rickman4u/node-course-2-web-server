0d49463a9bb6a73cac4bb8c9061e8ccd1ee145a9
find4u <richard.git@find4u.co.uk>
2018-05-01 13:55:59 +0100

    setup port var for heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-
+const port = process.env.PORT || 3000;
 var app = express();
 
 
@@ -78,4 +78,4 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000,() => {console.log('Server started on port 3000');});
+app.listen(port,() => {console.log(`Server started on port ${port}`);});
